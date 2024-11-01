fae60574ddc8ceeab7b00447893d852013b7b3e1
Hellennjerik <hellen.karanja@strathmore.edu>
2024-11-01 21:04:52 +0300

    person

--- person.py
+++ person.py
@@ -1,10 +1,9 @@
 # person.py
 
 class Person:
-    def __init__(self, name, age, gender):
+    def __init__(self, name, age, ):
         self.name = name
         self.age = age
-        self.gender = gender
 
     def get_info(self):
         return f"Name: {self.name}, Age: {self.age}, Gender: {self.gender}"
